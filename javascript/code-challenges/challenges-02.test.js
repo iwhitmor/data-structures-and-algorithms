604632b8ee3406c1f773019098630cc210a05cf5
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-01 10:21:25 -0500

    challenge 5 is now working

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -69,8 +69,14 @@ CHALLENGE 5
 Write a function named forEachTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 4, but uses forEach instead of a for loop.
 ------------------------------------------------------------------------------------------------ */
 
-const forEachTwoToThe = (arr) => {
-  // Solution code here...
+const forEachTwoToThe = (integers) => {
+  let result = [];
+  integers.forEach(integer => {
+    let twoPower = Math.pow(2, integer);
+    result.push(twoPower);
+  });
+
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -244,7 +250,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forEachTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forEachTwoToThe([0, 4, 5]).length).toStrictEqual(3);
