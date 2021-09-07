8fa2d97c4ae8e720da6fe15ea87f015b64dca6b9
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-07 10:22:52 -0500

    working through challenge 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -41,9 +41,10 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 ------------------------------------------------------------------------------------------------ */
 
-const addPurchases = (arr) => {
-  // Solution code here...
-};
+const addPurchases = (objects => {
+  return objects.reduce(
+    (item, purchasePrice) => item + purchasePrice, 0);
+});
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -300,7 +301,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add the purchase price', () => {
     expect(addPurchases([{item: 'switch', purchasePrice: 399}, {item: 'toothpaste', purchasePrice: 2}])).toStrictEqual(401);
     expect(addPurchases([])).toStrictEqual(0);
