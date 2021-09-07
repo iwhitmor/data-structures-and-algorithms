1679773cc7b8788b0c4e6c12d57e76305efd1607
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-07 09:39:40 -0500

    challenge 1 is now working

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -12,7 +12,9 @@ Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
 const toLastNames = people => {
-  
+  return people.map(person => {
+    return person.firstName + ' ' + person.lastName;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
