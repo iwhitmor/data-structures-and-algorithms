7d14fdd0b5015a7058cff7aa5efdb46e95b879c2
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-27 17:03:29 -0500

    comment out

--- c-sharp/DataStructures/CodeChallenges/TreeFizzBuzz.cs
+++ c-sharp/DataStructures/CodeChallenges/TreeFizzBuzz.cs
@@ -1,10 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataStructures.Trees;
+using DataStructures;
+
 namespace CodeChallenges
 {
   public class TreeFizzBuzz
   {
-    public TreeFizzBuzz()
-    {
+   public static BinaryTree<string> FizzBuzz(BinaryTree<int> numberTree)
+   {
+     BinaryTree<string> result = new BinaryTree<string>();
+
+      //if (numberTree.Root == null)
+
+      //  yield break;
+
+      //foreach (int node in numberTree.PreOrder())
+      //{
+
+      //  if (node % 3 == 0)
+      //  {
+      //    return ("Fizz");
+      //  }
+
+      //  else if (node % 5 == 0)
+      //  {
+      //    return ("Buzz");
+      //  }
+
+      //  else if (node % 3 == 0 && node % 5 == 0)
+      //  {
+      //    return ("FizzBuzz");
+      //  }
+
+      //  else return node;  
+      //  }
+
+      //yield return result;
+
     }
   }
 }
