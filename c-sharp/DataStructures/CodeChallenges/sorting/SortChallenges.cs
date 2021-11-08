21488e0d815f0c49ecb268253470d1d3f3dad27a
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-08 14:12:59 -0600

    sort insertion code from pseudocode

--- c-sharp/DataStructures/CodeChallenges/sorting/SortChallenges.cs
+++ c-sharp/DataStructures/CodeChallenges/sorting/SortChallenges.cs
@@ -3,6 +3,29 @@ namespace CodeChallenges.sorting
 {
   public class SortChallenges
   {
-    
+    public void sort(int[] arr)
+    {
+
+      for (int i = 1; i < arr.length, i++)
+
+      {
+
+        int j = i - 1;
+
+        int temp = arr[i];
+
+        while (j >= 0 && temp < arr[j])
+
+        {
+
+          arr[j + 1] = arr[j]
+            j = j - 1;
+
+        }
+
+        arr[j + 1] == temp;
+
+      }
+    }
   }
 }
