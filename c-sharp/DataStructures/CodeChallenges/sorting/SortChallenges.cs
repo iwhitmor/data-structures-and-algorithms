512a5400608d8adcbbc0c70c0ee0163ced11ad8f
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-14 13:27:23 -0600

    add merge sort

--- c-sharp/DataStructures/CodeChallenges/sorting/SortChallenges.cs
+++ c-sharp/DataStructures/CodeChallenges/sorting/SortChallenges.cs
@@ -28,9 +28,55 @@ namespace CodeChallenges.sorting
       }
     }
 
-    public static void MergeSort(int[] arr)
+    public static void MergeSort(int[] array)
     {
-      
-    }  
+      int n = array.Length;
+
+      if (n > 1)
+      {
+        int mid = (n / 2);
+        int[] left = new int[mid];
+        int[] right = new int[n-mid];
+
+        Array.Copy(array, left, mid);
+        Array.Copy(array, mid, right, 0, right.Length);
+
+        MergeSort(left);
+        MergeSort(right);
+
+        Merge(left, right, array);
+      }
+    }
+
+    public static void Merge(int[] left, int[] right, int[] array)
+    {
+      int i = 0;
+      int l = 0;
+      int r = 0;
+
+      while (l < left.Length && r < right.Length)
+      {
+        if (left[i] <= right[l])
+        {
+          array[r] = left[i];
+        }
+        else
+        {
+          array[r] = right[l];
+
+          l++;
+        }
+
+        r++;
+      }
+      if (i == left.Length)
+      {
+        array[i] = right[i];
+      }
+      else
+      {
+        array[i] = left[i];
+      }
+    }
   }
 }
