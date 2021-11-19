0c315297351d4a16bf618eac9f74a8dd76e4aaef
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-19 15:49:00 -0600

    add starter code for Graph

--- c-sharp/DataStructures/DataStructures/Graph/Graph.cs
+++ c-sharp/DataStructures/DataStructures/Graph/Graph.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Collections.Generic;
+
 namespace DataStructures.Graph
 {
   public class Graph
   {
-   
+    public List<GraphNode> Nodes;
+
+    public GraphNode AddNode()
+    {
+
+    }
+  }
+
+  public class GraphNode
+  {
+    string Value;
+
+    List<GraphEdge> Neighbors;
+  }
+
+  internal class GraphEdge
+  {
+    GraphNode From;
+    GraphNode To;
+    int Weight;
   }
 }
